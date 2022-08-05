23061c54c396427b5ff904cbda62a858b719d4f8
DanielVillota16 <danielvillota1016@gmail.com>
2022-08-04 19:21:43 -0500

    added functional component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Example nombre="Daniel" apellido="Villota"></Example>
   );
+}
+
+function Example(props){
+  return <h1>{`Hello ${props.nombre} ${props.apellido}!`}</h1>;
 }
 
 export default App;
